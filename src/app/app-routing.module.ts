73a696eca372fdbf29a982ab593f19980790b3da
Martin <lefils.martin@gmail.com>
2023-12-15 10:35:31 +0100

    3.1 - correction route page not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { PageHomeComponent } from './pages/page-home/page-home.component';
 import { PageMyPlantsComponent } from './pages/page-my-plants/page-my-plants.component';
 import { PageAdminComponent } from './pages/page-admin/page-admin.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: '', component: PageHomeComponent },
   { path: 'plants', component: PageMyPlantsComponent },
   { path: 'admin', component: PageAdminComponent },
+  { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
