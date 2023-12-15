f3b3a61c70c2513ce07c7ea6803162cabaa96b49
Martin <lefils.martin@gmail.com>
2023-12-15 09:40:58 +0100

    3 - routing principal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageHomeComponent } from './pages/page-home/page-home.component';
+import { PageMyPlantsComponent } from './pages/page-my-plants/page-my-plants.component';
+import { PageAdminComponent } from './pages/page-admin/page-admin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PageHomeComponent },
+  { path: 'plants', component: PageMyPlantsComponent },
+  { path: 'admin', component: PageAdminComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
